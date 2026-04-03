93ec558c6a15b9fcae426ea8b11cd61be88b5a40
lenoa7vo-png <lenoa7vo@gmail.com>
2026-04-04 01:15:57 +0800

    adding health and collision of weapon and enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,20 +1,21 @@
 extends CharacterBody2D
-
 class_name Enemy
+
 @export var pivotPoint:Node2D
+@onready var hitbox:Area2D = $Area2D
 @export var rotateSpeed:float = -0.2
 @export var radius:float = 0
 var rotateAngle:float = 0
+var health:int = 15
 
 func _ready() -> void:
 	if pivotPoint == null:
 		pivotPoint = $"../../PivotPoint"
-	
-	#radius = position.distance_to(pivotPoint.position)
-	#UpdateRotation()
+	if hitbox == null:
+		hitbox = $CollisionShape2D
 
 func _process(delta: float) -> void:
-	rotateAngle += rotateSpeed * delta
+	rotateAngle += rotateSpeed * delta		
 	
 	if rotateAngle > PI * 2:
 		rotateAngle -= PI * 2
@@ -26,8 +27,13 @@ func UpdateRotation():
 		cos(rotateAngle) * radius, 
 		sin(rotateAngle) * radius)
 	position.x += -radius
-	print_debug("new position", position, rotateAngle, radius)
-
+	#print_debug("new position", position, rotateAngle, radius)
 
 func _on_pivot_point_ready() -> void:
 	pass # Replace with function body.
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.name == "weapon" or area.name == "Area2D":
+		health -= 5
+		print_debug("health is ", health)
+	#print_debug(area.name)
