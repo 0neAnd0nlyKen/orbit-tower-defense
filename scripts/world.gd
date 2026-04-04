683d64a8e8f0274d49ea452fed95dab56b3d4f6f
lenoa7vo-png <lenoa7vo@gmail.com>
2026-04-04 16:38:22 +0800

    game restarts when score reaches cap

--- scripts/world.gd
+++ scripts/world.gd
@@ -2,14 +2,25 @@ extends Node
 
 var coins:int = 0
 var score:int = 0
-var goal:int
+var goal:int = 20
+var countdown:float = 0.0
 
 func _ready() -> void:
-	goal = 20
+	pass
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if score >= goal:
-		get_tree().paused = true
+		#get_tree().paused = true
+		countdown += delta
+		if countdown >= 3.0:
+			levelUp()
 
 func _on_spawner_child_exiting_tree(_node: Node) -> void:
 	score += 1
+	print_debug(score)
+
+func levelUp():
+	#get_tree().paused = false
+	get_tree().reload_current_scene()
+	goal += goal%2
+	print_debug(goal)
