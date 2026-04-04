e8caba4363c7ab922de9e5b1e6381c06c8f6fc48
0neAnd0nlyKen <kendrickraphael@gmail.com>
2026-04-04 15:14:19 +0800

    main menu prototype

---
+++ scripts/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+func _on_play_button_button_down() -> void:
+	play_game() # JALANKAN GAME
+	
+func play_game():
+	pass
+
+
+func _on_quit_button_button_down() -> void:
+	quit_game() # KELUAR GAME
+	
+func quit_game():
+	pass
